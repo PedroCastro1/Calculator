d1091d8a107b3b6dab305beef9b309c7168422ed
Pedro <pedro.silva.013@acad.pucrs.br>
2018-08-27 20:01:24 -0300

    Arrumar testes parametrizados

--- src/main/java/pcastro/Calculator/Calculator.java
+++ src/main/java/pcastro/Calculator/Calculator.java
@@ -1,20 +1,38 @@
 package pcastro.Calculator;
 
 public class Calculator {
-	  public int evaluate(String expression) {
-	    String tokens[] = expression.split("(?<=\\+)|(?=\\+)|(?<=\\-)|(?=\\-)");
-	    int result = Integer.parseInt(tokens[0]);
-	    
-	    for(int i = 1; i < tokens.length-1; i+=2) {
-	    	switch(tokens[i]) {
-	    	case "+":
-	    		result += Integer.parseInt(tokens[i+1]);
-	    		break;
-	    	case "-":
-	    		result -= Integer.parseInt(tokens[i+1]);
-	    		break;
-	    	}
-	    }
-	    return result;
-	  }
+	public int evaluate(String expression) {
+		String tokens[] = expression.split("(?<=\\+)|(?=\\+)|(?<=\\-)|(?=\\-)");
+		
+		int primeiro = 0;
+		int result = 0;
+        
+		// Trata o sinal inicial
+		String aux = tokens[primeiro];
+		if (aux.equals("-")) {
+			primeiro++;
+			result -= Integer.parseInt(tokens[primeiro]);
+			primeiro++;
+		}else if (aux.equals("+")) {
+			primeiro++;
+			result += Integer.parseInt(tokens[primeiro]);
+			primeiro++;
+		}else {
+			result += Integer.parseInt(aux);
+			primeiro++;
+		}
+		
+		// Trata a sequencia
+		for(int i=primeiro;i<tokens.length;i+=2) {
+			switch(tokens[i]) {
+			case "+":
+				result += Integer.parseInt(tokens[i+1]);
+				break;
+			case "-":
+				result -= Integer.parseInt(tokens[i+1]);
+				break;
+			}
+		}
+		return result;
+	}
 }
--- src/test/java/pcastro/Calculator/CalculatorTest.java
+++ src/test/java/pcastro/Calculator/CalculatorTest.java
@@ -1,27 +1,19 @@
 package pcastro.Calculator;
 
-import static org.junit.Assert.*;
-
-import org.junit.Before;
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 
+@RunWith (JUnitParamsRunner.class)
 public class CalculatorTest {
-	private Calculator c;
-	
-	@Before
-	
-	public void inicializa() {
-		c = new Calculator();
-	}
 	@Test
-	public void calculatorVariasSomasTest() {
-		int resp = c.evaluate("5+3");
-		assertEquals(8,resp);
-	}
-	
-	@Test
-	public void calculatorSoumNroTest() {
-		int resp = c.evaluate("5");
-		assertEquals(5,resp);
+	@Parameters ({"1+2+3, 6", "1-1+1, 1", "-2-2-2, -4", "3, 3", "-3, -3", "5-5, 0"})
+	//Arrumar testes parametrizados
+	public CalculatorTest(String val, int resp) {
+		Calculator c = new Calculator();
+		int observed = c.evaluate(val);
+		assertEquals(observed, resp);
 	}
 }
