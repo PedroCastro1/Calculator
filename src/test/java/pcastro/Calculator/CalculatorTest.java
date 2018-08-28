0fc4010b305deb3732d04994803a612e31e17b9b
Pedro <pedro.silva.013@acad.pucrs.br>
2018-08-27 23:46:12 -0300

    Calculator final

--- src/test/java/pcastro/Calculator/CalculatorTest.java
+++ src/test/java/pcastro/Calculator/CalculatorTest.java
@@ -1,5 +1,4 @@
 package pcastro.Calculator;
-
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,9 +8,8 @@ import junitparams.Parameters;
 @RunWith (JUnitParamsRunner.class)
 public class CalculatorTest {
 	@Test
-	@Parameters ({"1+2+3, 6", "1-1+1, 1", "-2-2-2, -4", "3, 3", "-3, -3", "5-5, 0"})
-	//Arrumar testes parametrizados
-	public CalculatorTest(String val, int resp) {
+	@Parameters ({"1+2+3, 6", "1-1+1, 1", "-2-2-2, -6", "3, 3", "-3, -3", "5-5, 0"})
+	public void CalculatorTest(String val, int resp) {
 		Calculator c = new Calculator();
 		int observed = c.evaluate(val);
 		assertEquals(observed, resp);
